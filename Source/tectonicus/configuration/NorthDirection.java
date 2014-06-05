6ac43f5215cae54fca58835b7414a64f25bd6fed
skoeven <skoeven@gmail.com>
2014-06-04 23:52:00 -0600

    Modify license header and copyright in all main Java source files Normalize line endings

--- Source/tectonicus/configuration/NorthDirection.java
+++ Source/tectonicus/configuration/NorthDirection.java
@@ -1,39 +1,12 @@
 /*
- * Source code from Tectonicus, http://code.google.com/p/tectonicus/
+ * Copyright (c) 2012-2014, John Campbell and other contributors.  All rights reserved.
  *
- * Tectonicus is released under the BSD license (below).
- *
- *
- * Original code John Campbell / "Orangy Tang" / www.triangularpixels.com
- *
- * Copyright (c) 2012, John Campbell
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice, this list
- *     of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above copyright notice, this
- *     list of conditions and the following disclaimer in the documentation and/or
- *     other materials provided with the distribution.
- *   * Neither the name of 'Tecctonicus' nor the names of
- *     its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This file is part of Tectonicus. It is subject to the license terms in the LICENSE file found in
+ * the top-level directory of this distribution.  The full list of project contributors is contained
+ * in the AUTHORS file found in the same location.
  *
  */
+
 package tectonicus.configuration;
 
 public enum NorthDirection
