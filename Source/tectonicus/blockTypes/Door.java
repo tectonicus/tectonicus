013f88bb4e5806c80f1a5dd9399e5354675738d6
skoeven <skoeven@gmail.com>
2014-09-08 04:01:24 -0600

    Doors are now textured correctly based on hinge position and whether the door is open or closed. Right-hinged doors now rotate correctly. Resolves #44

--- Source/tectonicus/blockTypes/Door.java
+++ Source/tectonicus/blockTypes/Door.java
@@ -40,12 +40,12 @@ public class Door implements BlockType
 		
 		final float uWidth; // fudge factor
 		if (topTexture.texturePackVersion =="1.4")
-			uWidth = 1.0f / 16.0f / 16.0f * 2.5f;
+			uWidth = 1.0f / 16.0f / 16.0f * 3f;
 		else
-			uWidth = 1.0f / 16.0f * 2.5f;
+			uWidth = 1.0f / 16.0f * 3f;
 		
 		this.edgeTexture = new SubTexture(topTexture.texture, topTexture.u0, topTexture.v0, topTexture.u0+uWidth, topTexture.v1);
-		this.topEdgeTexture = new SubTexture(topTexture.texture, topTexture.u0, topTexture.v0, topTexture.u1, topTexture.v0+uWidth);
+		this.topEdgeTexture = new SubTexture(bottomTexture.texture, bottomTexture.u1-uWidth, bottomTexture.v0, bottomTexture.u1, bottomTexture.v1);
 	}
 	
 	@Override
@@ -79,17 +79,24 @@ public class Door implements BlockType
 		
 		final int data = rawChunk.getBlockData(x, y, z);
 		final boolean isTop = (data & 0x8) > 0;
+		int hingePos = data & 0x1;
 		boolean isOpen = (data & 0x4) > 0;
-		int hingePos = data & 0x3;
+		int facing = data & 0x3;
 		
 		if(isTop)
 		{
 			final int bottomData = rawChunk.getBlockData(x, y-1, z);
 			isOpen = (bottomData & 0x4) > 0;
-			hingePos = bottomData & 0x3;
+			facing = bottomData & 0x3;
 		}
+		else {
+			final int topData = rawChunk.getBlockData(x, y+1, z);
+			hingePos = topData & 0x1;
+		}
+			
 		
 		SubMesh subMesh = new SubMesh();
+		SubMesh doorTopMesh = new SubMesh();
 		
 		SubTexture frontTexture = isTop ? topTexture : bottomTexture;
 		
@@ -98,27 +105,31 @@ public class Door implements BlockType
 		Vector4f white = new Vector4f(lightness, lightness, lightness, 1);
 		
 		// Front face
-		subMesh.addQuad(new Vector3f(0, 1, thickness), new Vector3f(1, 1, thickness), new Vector3f(1, 0, thickness), new Vector3f(0, 0, thickness),
+		if (hingePos == 0){ //Hinge on the left
+			subMesh.addQuad(new Vector3f(0, 1, thickness), new Vector3f(1, 1, thickness), new Vector3f(1, 0, thickness), new Vector3f(0, 0, thickness),
 						white, frontTexture);
+		}
+		else {  //Hinge on the right
+			subMesh.addQuad(new Vector3f(0, 1, thickness), new Vector3f(1, 1, thickness), new Vector3f(1, 0, thickness), new Vector3f(0, 0, thickness),
+					white, new Vector2f(frontTexture.u1, frontTexture.v0), new Vector2f(frontTexture.u0, frontTexture.v0),new Vector2f(frontTexture.u0, frontTexture.v1), new Vector2f(frontTexture.u1, frontTexture.v1));
+		}
 		
 		// Back face
+		if (hingePos == 0){ //Hinge on the left
 		subMesh.addQuad(new Vector3f(1, 1, 0), new Vector3f(0, 1, 0), new Vector3f(0, 0, 0), new Vector3f(1, 0, 0), white,
 						new Vector2f(frontTexture.u1, frontTexture.v0), new Vector2f(frontTexture.u0, frontTexture.v0),new Vector2f(frontTexture.u0, frontTexture.v1), new Vector2f(frontTexture.u1, frontTexture.v1) );
+		}
+		else {  //Hinge on the right
+			subMesh.addQuad(new Vector3f(1, 1, 0), new Vector3f(0, 1, 0), new Vector3f(0, 0, 0), new Vector3f(1, 0, 0), white, frontTexture );
+		}
 		
 		// Top edge
-		subMesh.addQuad(new Vector3f(0, 1, 0), new Vector3f(1, 1, 0), new Vector3f(1, 1, thickness), new Vector3f(0, 1, thickness),
-						white, topEdgeTexture);
+		/*if (isTop) {
+			doorTopMesh.addQuad(new Vector3f(0, 1, 0), new Vector3f(1, 1, 0), new Vector3f(1, 1, thickness), new Vector3f(0, 1, thickness), white, 
+						new Vector2f(topEdgeTexture.u1, topEdgeTexture.v0), new Vector2f(topEdgeTexture.u1, topEdgeTexture.v1),new Vector2f(topEdgeTexture.u0, topEdgeTexture.v1), new Vector2f(topEdgeTexture.u0, topEdgeTexture.v0));
+		}*/
 		
-		// Hinge edge
-		subMesh.addQuad(new Vector3f(0, 1, 0), new Vector3f(0, 1, thickness), new Vector3f(0, 0, thickness), new Vector3f(0, 0, 0),
-						white, edgeTexture);
-		
-		// Non-hinge edge
-		subMesh.addQuad(new Vector3f(1, 1, thickness), new Vector3f(1, 1, 0), new Vector3f(1, 0, 0), new Vector3f(1, 0, thickness),
-				white, edgeTexture);
-		
-		// Now rotate depending on hinge position and open flag
-		
+		// Now rotate depending on door facing direction and open flag
 		Rotation rotation = Rotation.None;
 		float angle = 0;
 		
@@ -127,70 +138,111 @@ public class Door implements BlockType
 		float xOffset = x;
 		float yOffset = y;
 		float zOffset = z;
-		
-		if(isTop)
-		{
-			//TODO: Have to get block data from bottom half of the door
-		}
-		
+
 		if (isOpen)
 		{
-			if (hingePos == 0)
-			{
-				// Hinge in north-east corner
-				// ...already correct
+			// Top edge
+			if (isTop) {
+				doorTopMesh.addQuad(new Vector3f(0, 1, 0), new Vector3f(1, 1, 0), new Vector3f(1, 1, thickness), new Vector3f(0, 1, thickness), white, 
+							new Vector2f(topEdgeTexture.u0, topEdgeTexture.v1), new Vector2f(topEdgeTexture.u0, topEdgeTexture.v0),new Vector2f(topEdgeTexture.u1, topEdgeTexture.v0), new Vector2f(topEdgeTexture.u1, topEdgeTexture.v1));
 			}
-			else if (hingePos == 1)
-			{
-				// Hinge in south-east corner
-				rotation = Rotation.AntiClockwise;
-				angle = 90;
+			
+			// Hinge edge
+			subMesh.addQuad(new Vector3f(0, 1, 0), new Vector3f(0, 1, thickness), new Vector3f(0, 0, thickness), new Vector3f(0, 0, 0),
+							white, edgeTexture);
+			
+			// Non-hinge edge
+			subMesh.addQuad(new Vector3f(1, 1, thickness), new Vector3f(1, 1, 0), new Vector3f(1, 0, 0), new Vector3f(1, 0, thickness),
+					white, new Vector2f(edgeTexture.u1, edgeTexture.v0), new Vector2f(edgeTexture.u0, edgeTexture.v0),new Vector2f(edgeTexture.u0, edgeTexture.v1), new Vector2f(edgeTexture.u1, edgeTexture.v1));
+			
+			if (hingePos == 0){  //Hinge on the left
+				if (facing == 0)  //Facing West
+				{
+					// ...already correct
+				}
+				else if (facing == 1)  //Facing North
+				{
+					rotation = Rotation.AntiClockwise;
+					angle = 90;
+				}
+				else if (facing == 2)  //Facing East
+				{
+					rotation = Rotation.Clockwise;
+					angle = 180;
+				}
+				else if (facing == 3)  //Facing South
+				{
+					rotation = Rotation.Clockwise;
+					angle = 90;
+				}
 			}
-			else if (hingePos == 2)
-			{
-				// Hinge in south-west corner
-				rotation = Rotation.Clockwise;
-				angle = 180;
-			}
-			else if (hingePos == 3)
-			{
-				// Hinge in north-west corner
-				rotation = Rotation.Clockwise;
-				angle = 90;
+			else { //Hinge on the right
+				if (facing == 0)  //Facing West
+				{
+					rotation = Rotation.Clockwise;
+					angle = 180;
+					
+				}
+				else if (facing == 1)  //Facing North
+				{
+					rotation = Rotation.Clockwise;
+					angle = 90;
+				}
+				else if (facing == 2)  //Facing East
+				{
+					// ...already correct
+				}
+				else if (facing == 3)  //Facing South
+				{
+					rotation = Rotation.AntiClockwise;
+					angle = 90;
+				}
 			}
 		}
-		else
+		else  //Door is closed
 		{
-			if (hingePos == 0)
+			// Top edge
+			if (isTop) {
+				doorTopMesh.addQuad(new Vector3f(0, 1, 0), new Vector3f(1, 1, 0), new Vector3f(1, 1, thickness), new Vector3f(0, 1, thickness), white, 
+							new Vector2f(topEdgeTexture.u1, topEdgeTexture.v0), new Vector2f(topEdgeTexture.u1, topEdgeTexture.v1),new Vector2f(topEdgeTexture.u0, topEdgeTexture.v1), new Vector2f(topEdgeTexture.u0, topEdgeTexture.v0));
+			}
+			
+			// Hinge edge
+			subMesh.addQuad(new Vector3f(0, 1, 0), new Vector3f(0, 1, thickness), new Vector3f(0, 0, thickness), new Vector3f(0, 0, 0),
+							white, new Vector2f(edgeTexture.u1, edgeTexture.v0), new Vector2f(edgeTexture.u0, edgeTexture.v0),new Vector2f(edgeTexture.u0, edgeTexture.v1), new Vector2f(edgeTexture.u1, edgeTexture.v1));
+			
+			// Non-hinge edge
+			subMesh.addQuad(new Vector3f(1, 1, thickness), new Vector3f(1, 1, 0), new Vector3f(1, 0, 0), new Vector3f(1, 0, thickness),
+					white, edgeTexture);
+			
+			if (facing == 0) //Facing West
 			{
-				// Hinge in north-east corner
 				rotation = Rotation.AntiClockwise;
 				angle = 90;
 				xOffset -= texel * 13;
 			}
-			else if (hingePos == 1)
+			else if (facing == 1) //Facing North
 			{
-				// Hinge in south-east corner
 				rotation = Rotation.Clockwise;
 				angle = 180;
 				zOffset -= texel * 13;
 			}
-			else if (hingePos == 2)
+			else if (facing == 2)  //Facing East
 			{
-				// Hinge in south-west corner
 				rotation = Rotation.Clockwise;
 				angle = 90;
 				xOffset += texel * 13;
 			}
-			else if (hingePos == 3)
+			else if (facing == 3)  //Facing South
 			{
-				// Hinge in north-west corner
 				zOffset += texel * 13;
 			}
 		}
 		
-		if(isTop)
+		if (isTop) {
 			subMesh.pushTo(geometry.getMesh(topTexture.texture, Geometry.MeshType.AlphaTest), xOffset, yOffset, zOffset, rotation, angle);
+			doorTopMesh.pushTo(geometry.getMesh(bottomTexture.texture, Geometry.MeshType.AlphaTest), xOffset, yOffset, zOffset, rotation, angle);
+		}
 		else
 			subMesh.pushTo(geometry.getMesh(bottomTexture.texture, Geometry.MeshType.AlphaTest), xOffset, yOffset, zOffset, rotation, angle);
 	}
