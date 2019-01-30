a45cf391bb18ef1dd0dd9cd88ff5c9a6f0f0ce7d
skoeven <skoeven@gmail.com>
2019-01-30 00:44:43 -0700

    Handle multiple maps and layers with leaflet

--- leaflet/leafletMap.js
+++ leaflet/leafletMap.js
@@ -1,24 +1,45 @@
 function main() {
-	var mymap = L.map('map_canvas', {crs: L.CRS.Simple, minZoom: 0, maxZoom: 5}).setView([100, -100], 0);
-	
 	L.TileLayer.Tectonicus = L.TileLayer.extend({
 		getTileUrl: function(coords) {			
 			var xBin = coords.x % 16;
 			var yBin = coords.y % 16;
 			
-			var url = "Map0/LayerA/Zoom"+coords.z+"/"+xBin+"/"+yBin+"/tile_"+coords.x+"_"+coords.y+"."+"png";
-			return url;
+			return this.mapId + "/" + this.layerId + "/Zoom"+coords.z
+				+"/"+xBin+"/"+yBin+"/tile_"+coords.x+"_"+coords.y+"."+this.imageFormat;
 		},
 		getAttribution: function() {
-			return 'Tectonicus';
+			return '<a href="https://github.com/tectonicus/tectonicus">Tectonicus</a> - ' + this.mapName;
+		},
+		initialize: function(mapId, layerId, imageFormat, mapName) {
+			this.mapId = mapId;
+			this.layerId = layerId;
+			this.imageFormat = imageFormat;
+			this.mapName = mapName;
 		}
 	});
 	
-	L.tileLayer.tectonicus = function() {
-		return new L.TileLayer.Tectonicus();
+	var baseMaps = {}
+	var layers = [];
+	for (var i = 0; i < contents.length; i++) {
+		var tecMap = contents[i];
+		for (var j = 0; j < tecMap.layers.length; j++) {
+			var layer = tecMap.layers[j];
+			var tileLayer = new L.TileLayer.Tectonicus(tecMap.id, layer.id, layer.imageFormat, tecMap.name);
+			if (baseMaps.hasOwnProperty(tecMap.name + " - " + layer.name)) {
+				baseMaps[tecMap.name + " - " + layer.name + j] = tileLayer;  //A hack to handle duplicate layer names in the layer control
+			} else {
+				baseMaps[tecMap.name + " - " + layer.name] = tileLayer;
+			}
+			layers.push(tileLayer);
+		}
 	}
+
+	var map = L.map('map_canvas', {crs: L.CRS.Simple, minZoom: 0, maxZoom: maxZoom}).setView([100, -100], 0);
+	layers[0].addTo(map);
 	
-	L.tileLayer.tectonicus().addTo(mymap);
+	if (layers.length > 1) {
+		L.control.layers(baseMaps).addTo(map);
+	}
 }
 
 window.onload = main;
