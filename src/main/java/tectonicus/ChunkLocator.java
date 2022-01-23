ddd6322eee8c2caca324f06667abd66ab8806dc1
skoeven <skoeven@gmail.com>
2022-01-22 23:57:02 -0700

    Some code cleanup

--- src/main/java/tectonicus/ChunkLocator.java
+++ src/main/java/tectonicus/ChunkLocator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Tectonicus contributors.  All rights reserved.
+ * Copyright (c) 2022 Tectonicus contributors.  All rights reserved.
  *
  * This file is part of Tectonicus. It is subject to the license terms in the LICENSE file found in
  * the top-level directory of this distribution.  The full list of project contributors is contained
@@ -82,7 +82,7 @@ public class ChunkLocator
 		}
 		catch (Exception e)
 		{
-			log.error("ChunkLocator: Error while trying to load chunk at ({}, {}) from region {}", chunkCoord.x, chunkCoord.z, region.getFile().getAbsolutePath(), e);
+			log.error("ChunkLocator: Error while trying to load chunk at ({}, {}) from region {}", chunkCoord.x, chunkCoord.z, region.getRegionFile().getAbsolutePath(), e);
 		}
 		
 		return chunk;
