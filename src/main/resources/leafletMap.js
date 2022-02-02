d7543eb4a7a2175eb6063161146a984760d6d19f
skoeven <skoeven@gmail.com>
2022-02-01 17:02:24 -0700

    Hack to fix Leaflet map grid lines

--- src/main/resources/leafletMap.js
+++ src/main/resources/leafletMap.js
@@ -60,3 +60,22 @@ var vm = new Vue({
 		}
 	}
 });
+
+/*
+ * Workaround for 1px lines appearing in some browsers due to fractional transforms
+ * and resulting anti-aliasing.
+ * https://github.com/Leaflet/Leaflet/issues/3575
+ */
+(function(){
+    var originalInitTile = L.GridLayer.prototype._initTile
+    L.GridLayer.include({
+        _initTile: function (tile) {
+            originalInitTile.call(this, tile);
+
+            var tileSize = this.getTileSize();
+
+            tile.style.width = tileSize.x + 1 + 'px';
+            tile.style.height = tileSize.y + 1 + 'px';
+        }
+    });
+})()
