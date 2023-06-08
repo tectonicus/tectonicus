63cdd9d1dea8a857182d6edbdf6fe3ffa8147930
Matej Darebnik <51075039+matej-darebnik@users.noreply.github.com>
2023-06-08 06:45:46 +0200

    hotfix for errors reading from directory

--- src/main/java/tectonicus/cache/swap/HddTileListIterator.java
+++ src/main/java/tectonicus/cache/swap/HddTileListIterator.java
@@ -145,15 +145,24 @@ public class HddTileListIterator implements Iterator<TileCoord>
 	
 	private void refreshTiles()
 	{
-		// Refresh the tiles array
-		tiles = fetchTiles(yDirs[yPosition]);
-		tilePosition = -1;
+                if (yDirs != null && yDirs.length > 0)
+                {
+                        // Refresh the tiles array
+                        tiles = fetchTiles(yDirs[yPosition]);
+                        tilePosition = -1;
+                }
 	}
 	
 	private void refreshYDir()
 	{
 		// Refresh the y dir
 		yDirs = xDirs[xPosition].listFiles( new DirectoryFilter() );
+                
+                if (yDirs == null)
+                {
+                    log.warn(String.format("Unable to read directories from directory %s", xDirs[xPosition].getAbsolutePath()));
+                }
+                
 		yPosition = 0;
 	}
 	
