cfac5c590de647f667296cc0aeeb006f6e218709
Matej Darebnik <51075039+matej-darebnik@users.noreply.github.com>
2024-04-01 18:49:51 +0200

    updated leaflet to 1.9.4

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -606,23 +606,4 @@ function findStartView(params, defaultLayerId, defaultSpawnPos) {
 	return new ViewPos(queryLayerId, queryPos, queryZoom, startPoint);
 }
 
-/*
- * Workaround for 1px lines appearing in some browsers due to fractional transforms
- * and resulting anti-aliasing.
- * https://github.com/Leaflet/Leaflet/issues/3575
- */
-(function(){
-    var originalInitTile = L.GridLayer.prototype._initTile
-    L.GridLayer.include({
-        _initTile: function (tile) {
-            originalInitTile.call(this, tile);
-
-            var tileSize = this.getTileSize();
-
-            tile.style.width = tileSize.x + 1 + 'px';
-            tile.style.height = tileSize.y + 1 + 'px';
-        }
-    });
-})()
-
 window.onload = main;
